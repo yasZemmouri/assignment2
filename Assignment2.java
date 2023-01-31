6daff676c27cf953b3c7497b6390b376dbc987b4
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2023-01-31 18:49:10 -0500

    1, 2, 3

---
+++ Assignment2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Assignment2{
+    public static void main(String[] args){
+        //1. Prompt for a line of input to be URL encoded
+        System.out.println("Enter a line of text to be URL encoded");
+        //2. Read the line of text into a String (using the Scanner class nextLine method)
+        //I forgot
+        Scanner read = new Scanner(System.in);
+        String input = read.nextLine();
+        //3. Print the line that was read.
+        System.out.println("The string read is: " + input);
+        
+
+    }
+}
